de108bdfef9962df32d1c773687b751e67a71608
Cornettoo <cornehuisman@outlook.com>
2018-12-22 16:00:17 +0100

    Text block

---
+++ flexible_content/text.php
@@ -0,0 +1,45 @@
+<?php
+// Block padding
+$padding_top = get_sub_field('padding_top');
+$padding_bottom = get_sub_field('padding_bottom');
+
+// Block options
+$options = get_sub_field('options');
+?>
+
+<section class="text padding-top--<?= $padding_top; ?> padding-bottom--<?= $padding_bottom; ?>">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-10 col-lg-8 offset-md-1 offset-lg-2">
+                <h2><?php the_sub_field('title'); ?></h2>
+                <?php 
+                the_sub_field('text'); 
+                
+                if ($options && in_array('columns', $options)) :
+                ?>
+                    <div class="text-columns">
+                        <?php
+                        while (have_rows('columns')) : the_row();
+                            ?>
+                            <div class="text-columns__column">
+                                <h4><?php the_sub_field('title'); ?></h4>
+                                <?php the_sub_field('text'); ?>
+                            </div>
+                            <?php
+                        endwhile;
+                        ?>
+                    </div>
+                    <?php
+                endif;
+
+                if ($options && in_array('button', $options)) :
+                    $button = get_sub_field('button');
+                    ?>
+                    <a href="<?= $button['url']; ?>" target="<?= $button['target']; ?>" class="button button--primary"><?= $button['title']; ?></a>
+                    <?php
+                endif;
+                ?>
+            </div>
+        </div>
+    </div>
+</section>
--- index.php
+++ index.php
@@ -18,4 +18,10 @@ get_header();
 </header>
 
 <?php
+if (have_rows('flexible_content')) :
+    while (have_rows('flexible_content')) : the_row();
+        include('flexible_content/' . get_row_layout() . '.php');
+    endwhile;
+endif;
+
 get_footer();
